bedb9f0ff50a7bc39afdd42517fec5bdedc83cf3
metinowy15 <9789672@gmail.com>
2018-02-28 17:33:09 +0100

    aaaaaaaaa

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
         } else if ($('.dzikipokemon-background-normalny').length == 1) {
             if ($('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=2"] span').html() == "0% PŻ") {
                 $('#skrot_leczenie').trigger('click');
-                $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=5"]').trigger('click');
+                $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=4"]').trigger('click');
             } else {
                 $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=2"]').trigger('click');
             }
@@ -67,6 +67,6 @@ $(document).ready(function() {
     });
 
     $(document).on("click", '#goAutoButton', function() {
-        window.repeat = setInterval(function() { click(); }, 1000);
+        window.repeat = setInterval(function() { click(); }, 500);
     });
 });
