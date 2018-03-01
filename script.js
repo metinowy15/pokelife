00ea5718c5b5fe878a0c8fe1ca569365bcb169cb
metinowy15 <9789672@gmail.com>
2018-03-01 21:40:32 +0100

    update

--- script.js
+++ script.js
@@ -1,79 +1,151 @@
 $(document).ready(function() {
+    $.wait = function(ms) {
+        var defer = $.Deferred();
+        setTimeout(function() { defer.resolve(); }, ms);
+        return defer;
+    };
+
     $('body').append('<div id="goDzicz" style="position: fixed; cursor: pointer; top: 0; right: 328px; z-index: 9999"></div>');
-    $('body').append('<div id="goButton" style="border-radius: 4px;position: fixed; cursor: pointer; top: 5px; right: 10px; font-size: 36px; text-align: center; width: 100px; height: 48px; line-height: 48px; background: ' + $('.panel-heading').css('background-color') + '; z-index: 9999">GO</div>');
-    $('body').append('<div id="goAutoButton" style="border-radius: 4px;position: fixed; cursor: pointer; top: 5px; right: 122px; font-size: 36px; text-align: center; width: 140px; height: 48px; line-height: 48px; background: ' + $('.panel-heading').css('background-color') + '; z-index: 9999">AutoGO</div>');
+    $('body').append('<div id="goButton" style="border-radius: 4px;position: fixed; cursor: pointer; top: 5px; right: 10px; font-size: 36px; text-align: center; width: 100px; height: 48px; line-height: 48px; background: '+$('.panel-heading').css('background-color')+'; z-index: 9999">GO</div>');
+    $('body').append('<div id="goAutoButton" style="border-radius: 4px;position: fixed; cursor: pointer; top: 5px; right: 122px; font-size: 36px; text-align: center; width: 140px; height: 48px; line-height: 48px; background: '+$('.panel-heading').css('background-color')+'; z-index: 9999">AutoGO</div>');
 
 
     var iconSelect;
 
-    iconSelect = new IconSelect("goDzicz", {
-        'selectedIconWidth': 48,
-        'selectedIconHeight': 48,
-        'selectedBoxPadding': 1,
-        'iconsWidth': 48,
-        'iconsHeight': 48,
-        'boxIconSpace': 1,
-        'vectoralIconNumber': 1,
-        'horizontalIconNumber': 6
-    });
-
+    iconSelect = new IconSelect("goDzicz",
+                {'selectedIconWidth':48,
+                'selectedIconHeight':48,
+                'selectedBoxPadding':1,
+                'iconsWidth':48,
+                'iconsHeight':48,
+                'boxIconSpace':1,
+                'vectoralIconNumber':1,
+                'horizontalIconNumber':6
+                });
     var icons = [];
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/gesty_zagajnik.jpg', 'iconValue':'gesty_zagajnik'});
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/dolina_wiatrakow.jpg', 'iconValue':'dolina_wiatrakow'});
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/koronny_szczyt.jpg', 'iconValue':'koronny_szczyt'});
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/potrojny_staw.jpg', 'iconValue':'potrojny_staw'});
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/pustynia_lodowa.jpg', 'iconValue':'pustynia_lodowa'});
-    //            icons.push({'iconFilePath':'http://poke-life.net/images/ikony/wielkie_bagna.jpg', 'iconValue':'wielkie_bagna'});
-
 
     $.each($('#pasek_skrotow li'), function(index, item) {
-        if ($(item).find('a').attr('href').substring(0, 9) == "gra/dzicz") {
-            icons.push({ 'iconFilePath': $(item).find('img').attr('src'), 'iconValue': $(item).find('a').attr('href').substring(28) });
+        if($(item).find('a').attr('href').substring(0,9) == "gra/dzicz"){
+            icons.push({'iconFilePath':$(item).find('img').attr('src'), 'iconValue':$(item).find('a').attr('href').substring(28)});
+        }
+    });
+    iconSelect.refresh(icons);
+    iconSelect.setSelectedIndex(1);
+
+    function click(){
+            if($('.dzikipokemon-background-shiny').length == 1){
+                console.log('spotkalem shiny');
+                $('#goButton').css('background', 'green');
+                window.auto = false;
+                $('#goAutoButton').html('AutoGO');
+            } else if($('.dzikipokemon-background-normalny').length == 1){
+                if($('#sidebar .stan-pokemon:nth-child(2)').find('.progress-bar').attr('aria-valuenow') < 500){
+                    console.log('lecze sie');
+                    $('#skrot_leczenie').trigger('click');
+                    $('button[href="dzicz.php?miejsce='+iconSelect.getSelectedValue()+'&wybierz_pokemona=5"]').trigger('click');
+                } else {
+                    console.log('wybieram pokemona 6');
+                    $('button[href="dzicz.php?miejsce='+iconSelect.getSelectedValue()+'&wybierz_pokemona=3"]').trigger('click');
+                }
+            } else if ($('button[href="dzicz.php?miejsce='+iconSelect.getSelectedValue()+'&zlap_pokemona=greatballe"]').length == 1){
+                console.log('rzucam greatballa');
+                $('button[href="dzicz.php?miejsce='+iconSelect.getSelectedValue()+'&zlap_pokemona=greatballe"]').trigger('click');
+            } else {
+                if($('.progress-stan2 div').attr('aria-valuenow') < 5){
+                    console.log('przerywam autoGo');
+                    window.auto = false;
+                    $('#goAutoButton').html('AutoGO');
+                } else {
+                    console.log('ide do dziczy '+iconSelect.getSelectedValue());
+                    $('#pasek_skrotow a[href="gra/dzicz.php?poluj&miejsce='+iconSelect.getSelectedValue()+'"] img').trigger('click');
+                }
+            }
+    }
+
+    $(document).off("click", "nav a");
+    $(document).on("click", "nav a", function(event) {
+        if($(this).attr('href').charAt(0)!='#' && !$(this).hasClass( "link" )) {
+            event.preventDefault();
+
+            //back_button
+            //Ucinanie " gra/ "
+            var new_buffer = $(this).attr('href');
+            new_buffer = new_buffer.substr(4);
+            remember_back(new_buffer);
+
+            $("html, body").animate({ scrollTop: 0 }, "fast");
+
+            //$("#glowne_okno").html(loadingbar);
+            $("#glowne_okno").load($(this).attr('href'), function(){
+               if(window.auto){
+                   setTimeout(function(){click();}, 300);
+               }
+            });
+
+            /* zamyka menu */
+            $('.collapse-hidefix').collapse('hide');
+
+            /* wyłącza aktywne menu */
+            //$('.nav li').removeClass('active');
         }
     });
 
+    $(document).off( "click", ".btn-akcja");
+    $(document).on( "click", ".btn-akcja", function(event) {
+        event.preventDefault();
 
 
-    iconSelect.refresh(icons);
+        if(this.id != 'back_button') {
 
-    iconSelect.setSelectedIndex(1);
-
-
-    function parseHealth() {
-        var link = $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=1"] span').html();
-        link = link.replace("% PŻ", "");
-        link = Number(link);
-        return link;
-
-    }
-
-    function click() {
-        if ($('.dzikipokemon-background-shiny').length == 1) {
-            $('#goButton').css('background', 'green');
-            clearInterval(window.repeat);
-        } else if ($('.dzikipokemon-background-normalny').length == 1) {
-            if (parseHealth() <= 70) {
-                $('#skrot_leczenie').trigger('click');
-                $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=4"]').trigger('click');
-            } else {
-                $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&wybierz_pokemona=1"]').trigger('click');
-            }
-        } else if ($('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&zlap_pokemona=greatballe"]').length == 1) {
-            $('button[href="dzicz.php?miejsce=' + iconSelect.getSelectedValue() + '&zlap_pokemona=greatballe"]').trigger('click');
         } else {
-            if ($('.progress-stan2 div').attr('aria-valuenow') < 5) {
-                clearInterval(window.repeat);
+            if($(this).prop('prev1') != '') {
+                $('#back_button').attr('href', $('#back_button').attr('prev1') );
+                $('#back_button').attr('prev1', $('#back_button').attr('prev2') );
+                $('#back_button').attr('prev2', $('#back_button').attr('prev3') );
+                $('#back_button').attr('prev3', $('#back_button').attr('prev4') );
+                $('#back_button').attr('prev4', $('#back_button').attr('prev5') );
+                $('#back_button').attr('prev5', '' );
             } else {
-                $('#pasek_skrotow a[href="gra/dzicz.php?poluj&miejsce=' + iconSelect.getSelectedValue() + '"] img').trigger('click');
+                $(this).prop('disabled',true);
             }
         }
-    }
 
-    $(document).on("click", '#goButton', function() {
+        //Obejście modali
+        if($('body').hasClass('modal-open')) {
+            $('body').removeClass('modal-open');
+            $('body').css({"padding-right":"0px"});
+            $('.modal-backdrop').remove();
+        }
+
+        $("html, body").animate({ scrollTop: 0 }, "fast");
+
+        //$("#glowne_okno").html(loadingbar);
+        $("#glowne_okno").load('gra/'+$(this).attr('href'), {limit: 20},
+        function (responseText, textStatus, req) {
+            if(window.auto){
+                setTimeout(function(){click();}, 300);
+            }
+            if (textStatus == "error") {
+                $("#glowne_okno").html(responseText);
+            }
+        });
+        //$("#glowne_okno").load($(this).attr('href'),{});
+    });
+
+    $(document).on("click", '#goButton', function(){
         click();
     });
 
-    $(document).on("click", '#goAutoButton', function() {
-        window.repeat = setInterval(function() { click(); }, 500);
+    window.auto = false;
+    $(document).on("click", '#goAutoButton', function(){
+        if(window.auto){
+            window.auto = false;
+            $('#goAutoButton').html('AutoGO');
+        } else {
+            window.auto = true;
+            $('#goAutoButton').html('STOP');
+            click();
+        }
     });
+
 });
